ca2e09d02f0c2536b49c239312731f1b618c13cc
ARK1309 <ameyakotian19@gmail.com>
2025-06-11 12:41:38 +0100

    Graph without colour

--- Challenge 1b.py
+++ Challenge 1b.py
@@ -2,6 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def challenge1B():
+    resolution = 1000  # How many points plotted for the range
+    freqMin = 405 #in THz
+    freqMax = 790 #in THz
+
+    
+    def formula(frequency):
+        n = (1 + (1 / (1.731 - (0.261 * frequency * 10 ** -3) ** 2)) ** 0.5) ** 0.5
+        return n
+    
+    frequencies = np.linspace(freqMin, freqMax, resolution)
+    n = formula(frequencies)
+
+    plt.plot(frequencies, n)
+    plt.title("Refractive index of water")
+    plt.xlabel("Frequency (THz)")
+    plt.ylabel("Refractive index, n")
+    plt.show()
+
+
 
 
     return None
