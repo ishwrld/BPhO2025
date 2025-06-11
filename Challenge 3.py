45eb59fcb1c41a7e7148bab34569392f4a9cb7be
Eshaal <100534150+ishwrld@users.noreply.github.com>
2025-06-11 20:10:34 +0100

    Attempt to implement slider

--- Challenge 3.py
+++ Challenge 3.py
@@ -1,21 +1,30 @@
 from matplotlib import pyplot as plt
-import math
 import numpy as np
+from matplotlib.widgets import Slider
+import math
 
-L = 2
-x = np.linspace(0, L, 1000)
 C = 345
 n = 1
 y = 1
 
+def update(val):
+    global L
+    L = length_Slider.val
+    line.set_ydata(formula(x))
+    plot1.canvas.draw_idle()
+
 def formula(x):
     return (np.sqrt((x)**2 + y**2) / (C/n)) + (np.sqrt((L - x)**2 + y**2) / (C/n))
 
-t = formula(x)
+ax_slider = plt.axes([0.2, 0.1, 0.65, 0.03])
+length_Slider = Slider(ax_slider, 'Change Length', 1, 5.0, valinit=4)
 
+plot1, plot2 = plt.subplots()
+plt.subplots_adjust(bottom=0.25)
+L = 2
+x = np.linspace(0, L, 1000)
+line, = plt.plot(x, formula(x), lw=1)
 
-plt.plot(x, t, linestyle='-', color='b', label='Data Points')
-plt.title("Test for Fermat's Principle")
-plt.xlabel("x (m)")
-plt.ylabel("Time (s)")
+length_Slider.on_changed(update)
+
 plt.show()
