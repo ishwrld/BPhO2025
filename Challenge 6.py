42077c3aa6ac9bcf76a3bbfe234c9964dd84df73
Eshaal <100534150+ishwrld@users.noreply.github.com>
2025-06-30 11:05:38 +0100

    Update Challenge 6.py

--- Challenge 6.py
+++ Challenge 6.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+import matplotlib.transforms as mtransforms
+
 fig, ax = plt.subplots()
 ax.set_ylim(-6,6)
 ax.set_xlim(-6,6)
@@ -11,11 +13,10 @@ plt.title("Image in a Converging Lense")
 image_path = r"Einstein.jpg"
 img = Image.open(image_path)
 img_array = np.array(img)
-imgT_array = np.array(img)
 
-imgT_extent = []
 
 ax.imshow(img_array, extent=[2,4,0,2], zorder=1)
+
 
 
 
