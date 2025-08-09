fa3c098e1a7538a8bafa1f47414eea6464c4e3fd
Eshaal <100534150+ishwrld@users.noreply.github.com>
2025-08-09 02:05:52 +0100

    Prism + Line, not adjusted

--- Challenge 12 DRAFT.py
+++ Challenge 12 DRAFT.py
@@ -9,11 +9,19 @@ plt.rcParams['axes.spines.bottom'] = False
 prism_x = [0, 0.5, 1, 0]
 prism_y = [0, np.sqrt(3)/2, 0, 0]
 
-plt.figure()
-plt.plot(prism_x, prism_y)
+incident_x = [-0.5, 0]
+incident_y = [0, (np.sqrt(3)/2)/2]
 
+fig = plt.figure(facecolor='black')
+ax = fig.add_subplot(111, facecolor='black')
 
-plt.xticks([])
-plt.yticks([])
+ax.plot(prism_x, prism_y, color="white")
+ax.plot(incident_x, incident_y, color="gainsboro")
+
+ax.set_xticks([])
+ax.set_yticks([])
+
+ax.legend(facecolor='black')
 
 plt.show()
+
