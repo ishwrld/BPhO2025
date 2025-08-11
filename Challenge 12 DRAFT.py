1bc729d6517de01c12e8087fefb76a8e29efd5f8
Eshaal <100534150+ishwrld@users.noreply.github.com>
2025-08-11 21:57:19 +0100

    incidence angle slider added

--- Challenge 12 DRAFT.py
+++ Challenge 12 DRAFT.py
@@ -2,13 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets import Slider
 
-alpha_init = 60
+alpha_init = 80
+theta_init = 30
 
 fig, ax = plt.subplots(figsize=(6, 6), facecolor='black')
 ax.set_facecolor('black')
-plt.subplots_adjust(bottom=0.25)
+plt.subplots_adjust(bottom=0.35)
 
-def draw_triangle(alpha):
+theta_incidence = theta_init
+
+def draw_triangle(alpha, theta):
     ax.clear()
     ax.set_facecolor('black')
     ax.set_xticks([])
@@ -32,22 +35,25 @@ def draw_triangle(alpha):
         color="white"
     )
 
-    ax.set_title(f"Theta = filler , Alpha = {alpha}", color="white")
+    ax.set_title(f"Theta = {theta} , Alpha = {alpha}", color="white")
 
-draw_triangle(alpha_init)
+draw_triangle(alpha_init, theta_init)
 
-ax_slider = plt.axes([0.2, 0.1, 0.6, 0.03], facecolor="gray")
-slider_alpha = Slider(ax_slider, 'Alpha', 60, 150, valinit=alpha_init, valstep=5)
-slider_alpha.label.set_color('white')  # make label visible
+ax_slider_alpha = plt.axes([0.2, 0.18, 0.6, 0.03], facecolor="gray")
+slider_alpha = Slider(ax_slider_alpha, 'Alpha', 45, 150, valinit=alpha_init, valstep=5)
+slider_alpha.label.set_color('white')
+
+ax_slider_theta = plt.axes([0.2, 0.1, 0.6, 0.03], facecolor="gray")
+slider_theta = Slider(ax_slider_theta, 'Theta', 10, 90, valinit=theta_init, valstep=5)
+slider_theta.label.set_color('white')
 
 def update(val):
-    draw_triangle(slider_alpha.val)
+    global theta_incidence
+    theta_incidence = slider_theta.val
+    draw_triangle(slider_alpha.val, theta_incidence)
     fig.canvas.draw_idle()
 
 slider_alpha.on_changed(update)
+slider_theta.on_changed(update)
 
 plt.show()
-
-
-
-
