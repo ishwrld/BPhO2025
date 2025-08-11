f3d7d0e792d3e40c856b9d37951f8de69cda1b5c
Eshaal <100534150+ishwrld@users.noreply.github.com>
2025-08-11 22:10:43 +0100

    added delta and theta t

--- Challenge 12 DRAFT.py
+++ Challenge 12 DRAFT.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets import Slider
 
-alpha_init = 80
+alpha_init = 60
 theta_init = 30
+n = 1.5
 
 fig, ax = plt.subplots(figsize=(6, 6), facecolor='black')
 ax.set_facecolor('black')
@@ -11,7 +12,7 @@ plt.subplots_adjust(bottom=0.35)
 
 theta_incidence = theta_init
 
-def draw_triangle(alpha, theta):
+def draw_triangle(alpha, theta_i):
     ax.clear()
     ax.set_facecolor('black')
     ax.set_xticks([])
@@ -25,9 +26,9 @@ def draw_triangle(alpha, theta):
     half_base = base_length / 2
     height = half_base / np.tan(beta_rad)
 
-    apex = (0, height)
-    left_base = (-half_base, 0)
-    right_base = (half_base, 0)
+    apex = np.array([0, height])
+    left_base = np.array([-half_base, 0])
+    right_base = np.array([half_base, 0])
 
     ax.plot(
         [left_base[0], apex[0], right_base[0], left_base[0]],
@@ -35,16 +36,44 @@ def draw_triangle(alpha, theta):
         color="white"
     )
 
-    ax.set_title(f"Theta = {theta} , Alpha = {alpha}", color="white")
+    midpoint = (apex + left_base) / 2
+
+    side_vec = left_base - apex
+    normal_vec = np.array([side_vec[1], -side_vec[0]])
+    normal_vec = normal_vec / np.linalg.norm(normal_vec)
+
+    theta_rad = np.deg2rad(theta_i)
+    incident_vec = (
+        np.cos(theta_rad) * normal_vec
+        + np.sin(theta_rad) * np.array([-normal_vec[1], normal_vec[0]])
+    )
+
+    length = 0.8
+    p1 = midpoint + incident_vec * length
+    p2 = midpoint
+    ax.plot([p1[0], p2[0]], [p1[1], p2[1]], color="cyan")
+
+    theta_i_rad = np.deg2rad(theta_i)
+    alpha_rad = np.deg2rad(alpha)
+
+    theta_t_rad = np.arcsin(
+        np.sqrt(n**2 - np.sin(theta_i_rad)**2) * np.sin(alpha_rad)
+        - np.sin(theta_i_rad) * np.cos(alpha_rad)
+    )
+    theta_t = np.rad2deg(theta_t_rad)
+
+    delta = theta_i + theta_t - alpha
+
+    ax.set_title(f"Theta = {theta_i:.1f} , Alpha = {alpha:.1f}", color="white")
 
 draw_triangle(alpha_init, theta_init)
 
 ax_slider_alpha = plt.axes([0.2, 0.18, 0.6, 0.03], facecolor="gray")
-slider_alpha = Slider(ax_slider_alpha, 'Alpha', 45, 150, valinit=alpha_init, valstep=5)
+slider_alpha = Slider(ax_slider_alpha, 'Alpha', 60, 150, valinit=alpha_init, valstep=5)
 slider_alpha.label.set_color('white')
 
 ax_slider_theta = plt.axes([0.2, 0.1, 0.6, 0.03], facecolor="gray")
-slider_theta = Slider(ax_slider_theta, 'Theta', 10, 90, valinit=theta_init, valstep=5)
+slider_theta = Slider(ax_slider_theta, 'Theta', 0, 90, valinit=theta_init, valstep=1)
 slider_theta.label.set_color('white')
 
 def update(val):
@@ -57,3 +86,4 @@ slider_alpha.on_changed(update)
 slider_theta.on_changed(update)
 
 plt.show()
+
