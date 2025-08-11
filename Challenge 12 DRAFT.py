70f43c1e8f6c139bece86ef74038bd1f56ae1121
Eshaal <100534150+ishwrld@users.noreply.github.com>
2025-08-11 21:50:07 +0100

    Interactive Triangle

--- Challenge 12 DRAFT.py
+++ Challenge 12 DRAFT.py
@@ -1,27 +1,53 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.widgets import Slider
 
-plt.rcParams['axes.spines.left'] = False
-plt.rcParams['axes.spines.right'] = False
-plt.rcParams['axes.spines.top'] = False
-plt.rcParams['axes.spines.bottom'] = False
+alpha_init = 60
 
-prism_x = [0, 0.5, 1, 0]
-prism_y = [0, np.sqrt(3)/2, 0, 0]
+fig, ax = plt.subplots(figsize=(6, 6), facecolor='black')
+ax.set_facecolor('black')
+plt.subplots_adjust(bottom=0.25)
 
-incident_x = [-0.5, 0]
-incident_y = [0, (np.sqrt(3)/2)/2]
+def draw_triangle(alpha):
+    ax.clear()
+    ax.set_facecolor('black')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_aspect('equal')
 
-fig = plt.figure(facecolor='black')
-ax = fig.add_subplot(111, facecolor='black')
+    beta = (180 - alpha) / 2
+    beta_rad = np.deg2rad(beta)
 
-ax.plot(prism_x, prism_y, color="white")
-ax.plot(incident_x, incident_y, color="gainsboro")
+    base_length = 2
+    half_base = base_length / 2
+    height = half_base / np.tan(beta_rad)
 
-ax.set_xticks([])
-ax.set_yticks([])
+    apex = (0, height)
+    left_base = (-half_base, 0)
+    right_base = (half_base, 0)
 
-ax.legend(facecolor='black')
+    ax.plot(
+        [left_base[0], apex[0], right_base[0], left_base[0]],
+        [left_base[1], apex[1], right_base[1], left_base[1]],
+        color="white"
+    )
+
+    ax.set_title(f"Theta = filler , Alpha = {alpha}", color="white")
+
+draw_triangle(alpha_init)
+
+ax_slider = plt.axes([0.2, 0.1, 0.6, 0.03], facecolor="gray")
+slider_alpha = Slider(ax_slider, 'Alpha', 60, 150, valinit=alpha_init, valstep=5)
+slider_alpha.label.set_color('white')  # make label visible
+
+def update(val):
+    draw_triangle(slider_alpha.val)
+    fig.canvas.draw_idle()
+
+slider_alpha.on_changed(update)
 
 plt.show()
+
+
+
 
