ba68b4af76d4eb9067b63b00bc9a18afc623f15e
ARK1309 <ameyakotian19@gmail.com>
2025-08-12 16:02:36 +0100

    Fully completed challenge 11d

--- Challenge 11d.py
+++ Challenge 11d.py
@@ -1,159 +1,127 @@
-from matplotlib.widgets import Slider
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 
-R = 3500
-H = 5
-FREQUENCIES = np.linspace(400, 750, 8)
+LW = 0.5
+FREQUENCIES = np.linspace(410, 750, 15)
 
-# Find n for given frequency
-def find_index_water(frequency):
+def findIndexWater(frequency):
     return (1 + (1 / (1.731 - (0.261 * frequency * 10 ** -3) ** 2)) ** 0.5) ** 0.5
 
-# Epsilon for primary rainbow
-def primary_minimum(n): # RETURNS RADIANS
+def secondaryElevation(n):
     angle = np.arcsin(((9 - n ** 2) / 8) ** 0.5)
     return np.pi - 6 * np.arcsin(np.sin(angle) / n) + 2 * angle
 
-# Epsilon for secondary rainbow
-def secondary_minimum(n): # RETURN RADIANS
+def primaryElevation(n):
     angle = np.arcsin(((4 - n ** 2) / 3) ** 0.5)
-    return 4 * np.arcsin(angle / n) - 2 * angle
+    return 4 * np.arcsin(np.sin(angle) / n) - 2 * angle
 
-# Y coordinate for any given x coordinate with epsilon and alpha
-def calc_y(px, eps, alpha): # NEEDS RADIANS
-    radius = R * np.sin(eps) * np.cos(alpha)
-    shift = radius - R * np.sin(eps - alpha) - H
-    return (radius ** 2 - px ** 2) ** 0.5 - shift
-
-# Maximum x before going below y-axis
-def get_xlim(eps, alp): # NEEDS RADIANS
-    return (R ** 2 * np.sin(eps - alp) * np.sin(eps + alp) + 
-            2 * H * R * np.cos(eps) * np.sin(alp) - H ** 2) ** 0.5
-
-# Maximum y (when x = 0)
-def get_ylim(eps, alp): # NEEDS RADIANS
-    return R * np.sin(eps - alp) + H
-
-def get_radius(eps, alp): # NEEDS RADIANS
-    return R * np.sin(eps) * np.cos(alp)
-
-def get_shift(eps, alp): # NEEDS RADIANS
-    return R * np.sin(eps) * np.cos(alp) - R * np.sin(eps - alp) - H
-
-def choose_colour(freq):
-    if freq < 405:
-        return [np.nan, np.nan, np.nan]                     # Lower than red
-    elif freq < 480:
-        return [(freq-405)/75,0,0]                          # Red
+def chooseColour(freq):
+    if freq < 405:  # Lower than red
+        return [np.nan, np.nan, np.nan]
+    if freq < 480:
+        return [(freq-405)/75,0,0]  # Red
     elif freq < 510:
-        return [1, ((freq-480)/30)*127/255, 0]              # Orange
+        return [1, ((freq-480)/30)*127/255, 0]  # Orange
     elif freq < 530:
-        return [1, 127/255 + ((freq-510)/20)*128/255, 0]    # Yellow
+        return [1, 127/255 + ((freq-510)/20)*128/255, 0]  # Yellow
     elif freq < 600:
-        return [1-(freq-530)/70, 1, 0]                      # Green
+        return [1-(freq-530)/70, 1, 0]  # Green
     elif freq < 620:
-        return [0, 1, (freq-600)/20]                        # Cyan    
+        return [0, 1, (freq-600)/20]  # Cyan    
     elif freq < 680:
-        return [0, 1-(freq-620)/60, 1]                      # Blue
+        return [0, 1-(freq-620)/60, 1]  # Blue
     elif freq < 790:
-        return [((freq-680)/110)*127/255, 0, 1]             # Violet
+        return [((freq-680)/110)*127/255, 0, 1]  # Violet
     else:
-        return [np.nan, np.nan, np.nan]                     # Higher than violet
+        return [np.nan, np.nan, np.nan]
 
-def plot_curve(freq, alpha): # alpha in RADIANS
-    colour = choose_colour(freq)        # Colour of specified line based on freq
-    n = find_index_water(freq)          # Refractive index
-    print(n)
-    eps1 = primary_minimum(n)           # Epsilon for primary
-    eps2 = secondary_minimum(n)         # Epsilon for secondary
-    print(eps1, eps2, alpha)
-    x_limit1 = get_xlim(eps1, alpha)    # X limits for primary rainbow
-    print(x_limit1)
-    x_limit2 =  get_xlim(eps2, alpha)   # X limits for secondary rainbow
-    print(x_limit2)
+def drawCurve(freq, alpha):
+    colour = chooseColour(freq)
+    n = findIndexWater(freq)
+    eps1 = primaryElevation(n)
+    eps2 = secondaryElevation(n)
 
-    draw_circle(eps1, alpha, colour)
-    draw_circle(eps2, alpha, colour)
+    x1, y1 = getCircleCoords(eps1, alpha) # Primary
+    x2, y2 = getCircleCoords(eps2, alpha) # Secondary
 
-    # x_coords1 = np.linspace(-x_limit1, x_limit1, 1000)  # X coords above y-axis for primary
-    # x_coords2 = np.linspace(-x_limit2, x_limit2, 1000)  # X coords above y-axis for secondary
-    # y_coords1 = calc_y(x_coords1, eps1, alpha)          # Y coords for primary
-    # y_coords2 = calc_y(x_coords2, eps2, alpha)          # Y coords for secondary
-    # ax.plot(x_coords1, y_coords1, color = colour, linewidth = 1, label = "Primary")
-    # ax.plot(x_coords2, y_coords2, color = colour, linewidth = 1, label = "Secondary")
+    ax.plot(x1, y1, color = colour, linewidth = LW)
+    ax.plot(x2, y2, color = colour, linewidth = LW)
 
-def draw_circle(eps, alp, colour):
-    centre_x = 0
-    centre_y = -(R * np.sin(eps) * np.cos(alp) - R * np.sin(eps - alp) - H)
-    radius = R * np.sin(eps) * np.cos(alp)
-    theta = np.linspace(0, np.pi, 100)
-    x = centre_x + radius * np.cos(theta)
+def getCircleCoords(eps, alp):
+    theta = np.linspace(0, 2 * np.pi, 360)
+    radius = range * np.sin(eps) * np.cos(alp)
+    centre_y = -(range * np.sin(eps) * np.cos(alp) - range * np.sin(eps - alp) - height)
+    # range * np.sin(eps - alp) + height - range * np.sin(eps) * np.cos(alp)
+    x = radius * np.cos(theta)
     y = centre_y + radius * np.sin(theta)
     mask = y >= 0
     x = x[mask]
     y = y[mask]
-    ax.plot(x, y, color = colour, linewidth=1)
+    return x, y    
 
-def plot_rainbows(solar_angle): # Solar angle in RADIANS
-    global ax
-    max_freq = FREQUENCIES.max()
-    n = find_index_water(max_freq)
-    max_eps = secondary_minimum(n)
-    print("Max elevation: ", max_eps)
-    x_max = get_xlim(max_eps, solar_angle)
-    print("X-axis limit: ", x_max)
-    y_max = get_ylim(max_eps, solar_angle)
-    print("Y-axis limit: ", y_max)
-    ax.set_xlim = (-x_max*1.1, x_max*1.1)
-    ax.set_ylim = (0, y_max * 1.1)
-
+def drawRainbows(solar_angle):
     for freq in FREQUENCIES:
-        plot_curve(freq, solar_angle)
+        drawCurve(freq, solar_angle)
+
+def findLimits(solar_angle):
+    # Set axis limits using max frequency
+    max_freq = FREQUENCIES.max()
+    n = findIndexWater(max_freq)
+    eps2 = secondaryElevation(n)
+    x_coords, y_coords = getCircleCoords(eps2, solar_angle)
+    return x_coords.max(), y_coords.max()
 
 def update(val):
-    global ax
-    solar_angle = np.deg2rad(solar_slider.val)
+    # global height, range
+    solar_deg = solar_slider.val
+    solar_rad = np.deg2rad(solar_deg)
+    # height = height_slider.val
+    # range = range_slider.val
     ax.clear()
     ax.set_title("Primary and Secondary Rainbows")
-    max_freq = FREQUENCIES.max()
-    n = find_index_water(max_freq)
-    max_eps = secondary_minimum(n)
-    print("Max elevation: ", max_eps)
-    x_max = get_xlim(max_eps, solar_angle)
-    print("X-axis limit: ", x_max)
-    y_max = get_ylim(max_eps, solar_angle)
-    print("Y-axis limit: ", y_max)
-    ax.set_xlim = (-x_max*1.1, x_max*1.1)
-    ax.set_ylim = (0, y_max * 1.1)
+    x_lim, y_lim = findLimits(solar_rad)
+    ax.set_xlim(-x_lim*1.1, x_lim*1.1)
+    ax.set_ylim(0, x_lim*1.1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+    drawRainbows(solar_rad)
 
-    #ax.set_xticks([])
-    #ax.set_yticks([])
-
-    plot_rainbows(solar_angle)
-
-# Initialise plot
-# def run_plot():
+# Initiliase plot
 fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.25)
+plt.subplots_adjust(bottom = 0.25) # Give space for slider
 ax.set_title("Primary and Secondary Rainbows")
-x_limit = 500
-y_limit = 100
-ax.set_xlim(-x_limit, x_limit)
-ax.set_ylim(0, y_limit)
 ax.set_aspect('equal')
-#ax.set_xticks([])
-#ax.set_yticks([])
-ax.spines[['top', 'right', 'left', 'bottom']].set_visible(False)
+ax.set_xticks([])
+ax.set_yticks([])
+ax.spines[['top', 'right', 'left', 'bottom']].set_visible(False) # Make all border invisible
 
 # Slider
-solar_angle = 5
+initAngle = 5
 solar_ax = plt.axes([0.25, 0.1, 0.65, 0.03])
-solar_slider = Slider(solar_ax, 'Solar Angle', 5, 50, valinit=solar_angle, valstep=1)
+solar_slider = Slider(solar_ax, "Solar Angle, α", 1, 45, valinit = initAngle, valstep=0.25)
 
-plot_rainbows(np.deg2rad(solar_angle))
+# Left slider for range. R
+range = 3500
+# range_ax_left = plt.axes([0.075, 0.25, 0.03, 0.65])
+# range_slider = Slider(range_ax_left, "Range, r", 0, 5000, valinit = range, orientation='vertical')
+
+# Right slider for height, H
+height = 5
+# height_ax_right = plt.axes([0.91, 0.25, 0.03, 0.65]) # (left, bottom, width, height)
+# height_slider = Slider(height_ax_right, "Height, h", 0, 100, valinit = height, orientation='vertical')
+
+# Set limits
+x_lim, y_lim = findLimits(np.deg2rad(initAngle))
+ax.set_xlim(-x_lim*1.1, x_lim*1.1)
+ax.set_ylim(0, x_lim*1.1)
+
+# Draw rainbow
+drawRainbows(np.deg2rad(initAngle))
 
 solar_slider.on_changed(update)
+# range_slider.on_changed(update)
+# height_slider.on_changed(update)
+
 plt.show()
-
-
